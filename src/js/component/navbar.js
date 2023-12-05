2e10769005f3aecb9da58e0429e940dbc5423e8a
John Pazda <pazda.john00@gmail.com>
2023-12-05 05:04:01 +0000

    added search functionality

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { signOut } from "firebase/auth";
 import { auth } from "../../firebase";
 import { AuthContext } from "../context/authContext";
+import { Navigate } from "react-router";
 
 export const Navbar = () => {
   const { currentUser } = useContext(AuthContext);
@@ -13,7 +14,13 @@ export const Navbar = () => {
       <div className="user">
         <img src={currentUser.photoURL} alt="profile picture"></img>
         <span>{currentUser.displayName}</span>
-        <button onClick={() => signOut(auth)}>logout</button>
+        <button
+          onClick={() => {
+            signOut(auth);
+          }}
+        >
+          logout
+        </button>
       </div>
     </div>
   );
--- src/js/component/search.js
+++ src/js/component/search.js
@@ -1,17 +1,101 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  getDoc,
+  setDoc,
+  doc,
+  updateDoc,
+  serverTimestamp,
+} from "firebase/firestore";
+import { db } from "../../firebase";
+import { AuthContext } from "../context/authContext";
 
 export const Search = () => {
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
+  const [err, setErr] = useState(false);
+  const { currentUser } = useContext(AuthContext);
+
+  const userRef = collection(db, "users");
+  const handleSearch = async () => {
+    const q = query(userRef, where("displayName", "==", username));
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        console.log(doc.id, "=>", doc.data());
+        setUser(doc.data());
+      });
+    } catch {
+      (err) => {
+        setErr(true);
+      };
+    }
+  };
+
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearch();
+  };
+
+  const handleSelect = async () => {
+    const combinedID =
+      currentUser.uid > user.uid
+        ? currentUser.uid + user.uid
+        : user.uid + currentUser.uid;
+    try {
+      const res = await getDoc(doc(db, "chats", combinedID));
+
+      if (!res.exists()) {
+        await setDoc(doc(db, "chats", combinedID), { messages: [] });
+
+        await updateDoc(doc(db, "userChats", currentUser.uid), {
+          [combinedID + ".userInfo"]: {
+            uid: user.uid,
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+          },
+          [combinedID + ".date"]: serverTimestamp(),
+        });
+
+        await updateDoc(doc(db, "userChats", user.uid), {
+          [combinedID + ".userInfo"]: {
+            uid: currentUser.uid,
+            displayName: currentUser.displayName,
+            photoURL: currentUser.photoURL,
+          },
+          [combinedID + ".date"]: serverTimestamp(),
+        });
+        console.log(user);
+      }
+    } catch (err) {
+      console.log(err);
+      setUser(null);
+      setUsername("");
+    }
+  };
+
   return (
     <div className="search">
       <div className="searchForm">
-        <input type="text" placeholder="Find user" />
+        <input
+          type="text"
+          placeholder="Find user"
+          onKeyDown={handleKey}
+          onChange={(e) => setUsername(e.target.value)}
+          value={username}
+        />
       </div>
-      <div className="userChat">
-        <img src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
-        <div className="userChatInfo">
-          <span>Jane</span>
+      {err && <span>User not found</span>}
+      {user && (
+        <div className="userChat" onClick={handleSelect}>
+          <img src={user.photoURL} alt="friend's avatar" />
+          <div className="userChatInfo">
+            <span>{user.displayName}</span>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
